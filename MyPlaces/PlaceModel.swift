98644341f5779610f5d75892e2e333053942e32f
ValentinaLuchinovich <valinkontakt11@bk.ru>
2021-11-28 18:19:57 +0300

    Custom TableViewCell

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Валентина Лучинович on 28.11.2021.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var description: String
+    var image: String
+}
