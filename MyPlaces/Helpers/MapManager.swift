70e6f10142a4a8546c4e39db1581bed05081a33e
ValentinaLuchinovich <valinkontakt11@bk.ru>
2021-12-09 01:32:19 +0300

    Refactoring

---
+++ MyPlaces/Helpers/MapManager.swift
@@ -0,0 +1,137 @@
+//
+//  MapManager.swift
+//  MyPlaces
+//
+//  Created by Валентина Лучинович on 09.12.2021.
+//
+
+import UIKit
+import MapKit
+
+class MapManager {
+    // Менеджер для управления действиями связанными с местоположением пользователя
+    let locationManager = CLLocationManager()
+    // Параметр для масштаба карты
+    private let regionInMetrs = 1000.0
+    
+    // Настройки маркера места
+    func setupPlacemark(place: Place, mapView: MKMapView) {
+        // Извлекаем адрес
+        guard let location = place.location else { return }
+        // Создаем экземпляр класса CLGeocoder, который отвечает за преобразование географических координат и названий
+        let geocoder = CLGeocoder()
+        // Определяем место по адресу переданному в параметры
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            // Проверяем не содержит ли объект error каких-либо данных
+            if let error = error {
+                print(error)
+                return
+            }
+            // Если ошибки нет то
+            guard let placemarks = placemarks else { return }
+            let placemark = placemarks.first
+            
+            //Описываем точку на карте
+            let annotation = MKPointAnnotation()
+            annotation.title = place.name
+            annotation.subtitle = place.location
+            
+            // Определяем местоположение маркера
+            guard let placemarkLocation = placemark?.location else { return }
+            // Если вышло получить местоположение маркера, то привязываем аннотацию к этой же точке на картe
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            // Задаем размер карты таким образом, чтобы на ней были видны все собранные аннотации
+            mapView.showAnnotations([annotation], animated: true)
+            //  Выделяем созданную аннотацию
+            mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
+    
+    // Проверяем включены ли службы геолокации
+    func checkLocationServices(mapView: MKMapView, segueIdentifire: String, closure: () -> ()) {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            checkLocationAutorization(mapView: mapView, segueIdentifire: segueIdentifire)
+            closure()
+        } else {
+            // Реализуем задержку появления контроллера иначе он не будет отображаться
+            // так как метод viewDidLoad загружается еще до того как экран отображен
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.alertLocation(title: "Службы геолокации отключены",
+                                   message: "Перейдите в настройки, чтобы включить службы геолокации")
+            }
+        }
+    }
+    
+    
+    
+    // Метод проверяет статус на разрешение использования геопозиции
+    func checkLocationAutorization(mapView: MKMapView, segueIdentifire: String) {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            if segueIdentifire == "getAddress" { showUserLocation(mapView: mapView) }
+            break
+        case .notDetermined:
+            // Запрос на использование геолокации
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // Реализуем задержку появления контроллера иначе он не будет отображаться
+            // так как метод viewDidLoad загружается еще до того как экран отображен
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.alertLocation(title: "Службы геолокации отключены",
+                                   message: "Перейдите в настройки, чтобы включить службы геолокации")
+            }
+            break
+        case .denied:
+            // Реализуем задержку появления контроллера иначе он не будет отображаться
+            // так как метод viewDidLoad загружается еще до того как экран отображен
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.alertLocation(title: "Службы геолокации отключены",
+                                   message: "Перейдите в настройки, чтобы включить службы геолокации")
+            }
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("Новое неизвестное значение")
+        }
+    }
+    
+    // Метод отвечает за фокус карты на местоположении пользователя
+    func showUserLocation(mapView: MKMapView) {
+        // Если у получается определить местоположение пользователя
+        if let location = locationManager.location?.coordinate {
+        // то определяем регион для позиционирования карты с местоположением пользователя в центре
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMetrs,
+                                            longitudinalMeters: regionInMetrs)
+            // Устонавливаем регион местоположения на экране
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    // Метод определяет координаты центра карты на котором установлен маркер
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        // широта
+        let latitude = mapView.centerCoordinate.latitude
+        // долгота
+        let longitude = mapView.centerCoordinate.longitude
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    
+    // Алерт контроллер для служб геолокации
+    private func alertLocation(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindow.Level.alert + 1
+        alertWindow.makeKeyAndVisible()
+        alertWindow.rootViewController?.present(alert, animated: true)
+    }
+}
---
+++ MyPlaces/ViewControllers/MapViewController.swift
@@ -0,0 +1,143 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Валентина Лучинович on 04.12.2021.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+import SwiftUI
+
+// Реализуем протокол для передачи данных из MapViewController в EditPlaceTableViewController
+protocol MapViewControllerDelegate {
+    func getAddress (_ address: String?)
+}
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var mapPinImage: UIImageView!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var doneButtone: UIButton!
+    
+    let mapManager = MapManager()
+    var mapViewControllerDelegate: MapViewControllerDelegate?
+    var place = Place()
+    var annotetionIdentifire = "annotetionIdentifire"
+    
+  
+    // Идентификатор определяющий какой метод нужно выбрать
+    var incomeSegueIdentifire = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMapView()
+        // Убираем из зоны видимости стандартные логотипы карты Apple
+        mapView.layoutMargins.bottom = -100
+        // По умолчанию делаем строку с адресом пустой
+        addressLabel.text = ""
+    }
+
+    // Переход на участок карты где находится пользователь
+    @IBAction func centerViewInUserLocation() {
+        mapManager.showUserLocation(mapView: mapView)
+    }
+    
+    // При нажатии выполняется передача данных из MapViewController в EditPlaceTableViewController
+    @IBAction func doneButtonePressed() {
+        mapViewControllerDelegate?.getAddress(addressLabel.text)
+        // Закрываем вьюконтроллер
+        dismiss(animated: true)
+    }
+    
+    // Нажатие на кнопку крестика будет закрывать вьюконтроллер
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+    // Метод для настройки карты в зависимости от свойства incomeSegueIdentifire
+    private func setupMapView() {
+        if incomeSegueIdentifire == "showPlace" {
+            mapManager.setupPlacemark(place: place, mapView: mapView)
+            // скрываеь mapPin при переходе по методу showPlace
+            mapPinImage.isHidden = true
+            // скрываем лейбл с адресом
+            addressLabel.isHidden = true
+            // скрываем кнопку готово
+            doneButtone.isHidden = true
+        }
+    }
+}
+
+// Протокол MKMapViewDelegate позволяет расширять возможности аннотаций на карте
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // Исключаем возможность, что данная аннотация является текущем местоположением пользователя
+        guard !(annotation is MKUserLocation) else { return nil}
+        // Объект представляющий вью с аннотацией на карте, который будет переиспользоваться
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotetionIdentifire) as? MKPinAnnotationView
+        //В случае если  у нас нет представления с аннотацией, которое можно переиспользовать - создаем новую
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotetionIdentifire)
+            // Присваем значение true, чтобы иметь возможность отобразить аннотацию ввиде баннера
+            annotationView?.canShowCallout = true
+        }
+        // Проверяем наличие изображения
+        if let imageData = place.imageData {
+            // Отображаем на баннере изображение
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            // Делаем углы изображение полукруглыми, как у баннера
+            imageView.layer.cornerRadius = 10
+            // Обрезаем изобраджение по границе закругленных углов image view
+            imageView.clipsToBounds = true
+            // Помещаем в image view само изображение
+            imageView.image = UIImage(data: imageData)
+            // Пропорционально обрезаем изображение по границам фрейма
+            imageView.contentMode = .scaleAspectFill
+            // Отображаем image view на баннере
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        return annotationView
+    }
+    
+    // Получаем адрес соответствующий центру экрана
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = mapManager.getCenterLocation(for: mapView)
+        // Преображаем координаты в адрес
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
+            // Проверяем объект error на содержимое
+            if let error = error {
+                print(error)
+                return
+            }
+            // Если ошибки нет - извлекаем массив меток
+            guard let placemarks = placemarks else { return }
+            let placemark = placemarks.first
+            // Извлекаем улицу и номер дома
+            let streetName = placemark?.thoroughfare
+            let buildNumber = placemark?.subThoroughfare
+            
+            // Обнавляем интервейс в основном потоке асинхронно
+            DispatchQueue.main.async {
+                if streetName != nil && buildNumber != nil {
+                self.addressLabel.text = "\(streetName!), \(buildNumber!)"
+                } else if streetName != nil {
+                    self.addressLabel.text = "\(streetName!)"
+                } else {
+                    self.addressLabel.text = ""
+                }
+            }
+        }
+    }
+}
+
+// Метод отслеживает в реальном времени изменение местоположения
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        mapManager.checkLocationAutorization(mapView: mapView, segueIdentifire: incomeSegueIdentifire)
+    }
+}
