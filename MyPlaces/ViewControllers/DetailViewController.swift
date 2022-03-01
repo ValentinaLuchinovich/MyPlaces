3e3ed783c9eab72e6bd23613465d09014e996561
ValentinaLuchinovich <valinkontakt11@bk.ru>
2022-03-01 12:58:58 +0300

    Создан макет DetailViewController

---
+++ MyPlaces/ViewControllers/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewController.swift
+//  MyPlaces
+//
+//  Created by Валентина Лучинович on 01.03.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
